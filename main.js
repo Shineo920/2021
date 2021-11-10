d6d6b4ec7b9857220c162dd1507deae481cf99d5
Shineo <liushineo97@gmail.com>
2021-11-10 20:09:50 +0800

    11/10

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 function disableselect(e) {
-    return false;
-  }
-  function reEnable() {
-    return true;
-  }
-  document.onselectstart = new Function("return false");
-  document.oncontextmenu = new Function("return false");
+  return false;
+}
+function reEnable() {
+  return true;
+}
+document.onselectstart = new Function("return false");
+document.oncontextmenu = new Function("return false");
 
-  if (window.sidebar) {
-    document.onmousedown = disableselect;
-    document.onclick = reEnable;
-  }
+if (window.sidebar) {
+  document.onmousedown = disableselect;
+  document.onclick = reEnable;
+}
+function startTimer(){
+console.log("timer started");
+if(document.getElementById("username").value.length!=0){
+  setTimeout(refresh, 5000);
+}
+}
+function refresh(){
+alert("5 seconds up");
+document.getElementById("username").value = "";
+location.reload();
+}
