c7b09c53091b42718c5fa477d7a98afa9ee31bc0
Shineo <liushineo97@gmail.com>
2021-11-25 09:17:37 +0800

    11/25

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function disableselect(e) {
   return false;
 }
@@ -14,7 +15,7 @@ if (window.sidebar) {
 function startTimer(){
 console.log("timer started");
 if(document.getElementById("username").value.length!=0){
-  setTimeout(refresh, 5000);
+  setTimeout(refresh, 7000);
 }
 }
 function refresh(){
@@ -22,3 +23,25 @@ alert("5 seconds up");
 document.getElementById("username").value = "";
 location.reload();
 }
+
+function validation(){
+if(document.getElementById("username").value.localeCompare("username@gmail.com")!=0 || document.getElementById("Password").value.localeCompare("password")!=0){
+  alert("Wrong password or Email");
+  sendEmail();
+}
+}
+  function sendEmail() {
+    Email.send({
+      Host: "smtp.gmail.com",
+      Username: "useruseralast@gmail.com",
+      Password: "112233",
+      To: 'liushineo97@gmail.com',
+      From: "useruseralast@gmail.com",
+      Subject: "Suspicious activity with your account",
+      Body: "Wrong password inserted",
+    })
+      .then(function (message) {
+        alert("mail sent successfully")
+      });
+      alert("Mail sent successfully!")
+  }
